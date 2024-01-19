1b0db638c65bb1d39fe27c952e78841c8ca72fd2
s0zzang <dlth8888@gmail.com>
2024-01-19 16:15:13 +0900

    add. 2일차

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,40 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+/* ----------------- 변수 가지고 놀기 ---------------- */
+let admin;
+let name;
+name = 'John';
+admin = name;
+alert(admin);
+
+/* ---------------- 올바른 이름 선택하기 --------------- */
+// 1. 행성의 이름을 값으로 가진 변수
+const PLANET = 'earth';
+const OUR_PLANET_NAME = 'Earth';
+// 2. 웹사이트 개발중, 현재 접속 중인 사용자의 이름을 저장하는 변수
+const userName = '이름';
+const currentOnlineUserName = '이름';
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+// - 갯수 별 상품 가격 계산하기
+let getCalcProductPriceByQuantity;
+// - 구매 제품 가격의 총 합
+let TotalPriceOfPurchasedProducts;
+let TotalProductPrice;
+// - 1년 기준 일(day)자 수
+const NUMBER_OF_DAYS_YEAR = 365;
+const DAY_PER_YEAR = 365;
+// - 구매 결제 여부
+let purchasePaymentStatus;
+let isPayment;
+// - 구매 결제 내역
+let PurchasePaymentDetails;
+let paymentList;
+// - 브랜드 접두사
+const BRNAD_PREFIX = 'nike';
+// - 오늘의 운세
+let luckOfToday;
+// - 상품 정보
+const productInfo = '';
